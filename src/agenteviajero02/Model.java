158ae357b66de68f309d77b18220b951c3c8f98f
Juan Ortiz <juan.ortizdlt@uanl.edu.mx>
2015-05-21 22:15:07 -0500

    New Counter

--- src/agenteviajero02/Model.java
+++ src/agenteviajero02/Model.java
@@ -1,14 +1,13 @@
 package agenteviajero02;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Model {
   StringBuilder sb = new StringBuilder();
-  int counter=0;
+  int counter=0,sizeTour=0;
   int idxtext = -1;
   int[][] array;
   ArrayList<ArrayList> matrix;
@@ -20,13 +19,13 @@ public class Model {
   //This method reads the file .txt, then is saved in a String Builder
   public void readDocument(){
       BufferedReader br = null;
-    
+
     try {
-     
+
      String linea;
-     
+
      br = new BufferedReader(new FileReader("gr17.tsp"));
-     
+
      while ((linea= br.readLine()) != null) {
       if (linea.compareTo("EDGE_WEIGHT_SECTION")==0){
         idxtext = counter+1;
@@ -37,7 +36,7 @@ public class Model {
       sb.append(linea+"\n");
       counter++;
     }
-    
+
   } catch (Exception e) {
    e.printStackTrace();
  } finally {
@@ -47,26 +46,26 @@ public class Model {
     ex.printStackTrace();
   }
   String[] values = sb.toString().split("\n");
-  
+
   ArrayList<String> list = new ArrayList<>();
-  
+
   for (int i = idxtext; i < values.length; i++) {
     list.add(values[i]);
   }
-  
+
   matrix = new ArrayList<>();
-  
+
   ArrayList<Integer> toinsert= new ArrayList<>();
-  
+
   for (String string : list) {
     String[] temp=string.split("\\s+");
-    
+
     for (String inner : temp) {
       if(inner.isEmpty())
         continue;
       Integer number = Integer.parseInt(inner);
       if(number==0){
-        
+
         toinsert.add(number);
         matrix.add(toinsert);
         toinsert = new ArrayList<>();
@@ -75,7 +74,7 @@ public class Model {
       }
     }
   }
-  
+
   for (int i = 0; i < matrix.size(); i++) {
     for (int j = 0; j < matrix.size(); j++) {
       if (j>=i){
@@ -85,8 +84,8 @@ public class Model {
       }
     }
   }
-  
-  
+
+
 }
   }
   //This method saves the file's information in a matrix
@@ -100,13 +99,14 @@ public class Model {
         }
       }
 }
-  
+
   //This method chooses the first road which is going to start the heuristic
   public int generateFirstRoad(){
-      return (0 + (int)(Math.random()*counter)); 
+      int var=counter-2;
+      return (0 + (int)(Math.random()*var));
   }
 
-  //This method is going to search the best next route 
+  //This method is going to search the best next route
   public void searchRoad(){
       int min=1000,value = 0,position=0,search;
       if(road.size()>1){
@@ -116,11 +116,11 @@ public class Model {
         }
         compareAndSelect();
         keys.clear();
-        map.clear();  
+        map.clear();
       }else{
           for(int i=0; i<array.length;i++){
               int index=(int) road.get(0);
-              value=array[index][i];          
+              value=array[index][i];
                if(value<min && value!=0){
                  min=value;
                  position=i;
@@ -134,24 +134,24 @@ public class Model {
                    array[j][k]=0;
               }
           }
-      } 
+      }
   }
 //This Method calls searchRoad method to asks about next roads until it has found all of them
   public void generateNextRoad(int firstRoad){
       road.add(firstRoad);
-      
+
         for(int j=0; j<array.length; j++){
               for(int k=0;k<array.length; k++){
                if(k==firstRoad)
                    array[j][k]=0;
               }
-          
+
           }
       for(int i=0; i<counter;i++){
       searchRoad();
       }
   }
-   
+
   //This method checks all available roads able to be selected
   public void checkNextRoad(int numberToCheck){
     int value=0,min=1000,stop=0;
@@ -159,7 +159,7 @@ public class Model {
         value=array[numberToCheck][i];
         if(value<min && value!=0){
             min=value;
-        } 
+        }
     }
     map.put(numberToCheck, min);
     keys.add(numberToCheck);
@@ -179,29 +179,31 @@ public class Model {
           int selected=0;
           for(int k=0; k<array.length; k++){
               int rand=array[finalKey][k];
-              
+
               if(rand==best && rand !=0){
                   selected=k;
                   selectArch(k);
                   break;
-              }         
+              }
           }
            for(int j=0; j<array.length; j++){
               for(int k=0;k<array.length; k++){
-               if(k==selected)
+               if(k==selected){
+                   sizeTour+=array[j][k];
                    array[j][k]=0;
+                 }
               }
-          
+
           }
   }
-  
+
   //This function selects the best option to insert the road, in which arch PROBLEM FOUNDED HERE
   public void selectArch(int roadToAdd){
       suma=new int[counter];
       try{
       int val1,val2,sumas=0;
       for(int i=0; i<road.size(); i++){
-          
+
           val1=(int)road.get(i);
           val2=(int)road.get(i+1);
           sumas=(arrayRecovery[val1][roadToAdd])+(arrayRecovery[roadToAdd][val2])-(arrayRecovery[val1][val2]);
